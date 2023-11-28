34dd6e36b1081eef736758933f628642a845253a
Puneet56 <puneetdhiman56@gmail.com>
2023-11-29 02:34:27 +0530

    testing diff and finite diff

--- gates/twice.go
+++ gates/twice.go
@@ -5,7 +5,7 @@ import (
 	"math/rand"
 )
 
-func Twice() {
+func Twice(derivative bool) {
 	data := [][]float64{
 		{0, 0},
 		{1, 2},
@@ -17,26 +17,56 @@ func Twice() {
 
 	lr := 1e-1
 
-	for i := 0; i < 10; i++ {
-		dw := cost_twice(data, w)
-		w -= lr * dw
-		fmt.Printf("cost %f | weight %f\n", cost_twice(data, w), w)
-	}
+	if derivative {
+		for i := 0; i < 100; i++ {
+			dw := cost_twice_derivative(data, w)
+			w -= lr * dw
+			fmt.Printf("cost %f | weight %f\n", cost_twice_derivative(data, w), w)
+		}
 
-	fmt.Println("-----------FINAL RESULTS------------")
+		fmt.Println("-----------FINAL RESULTS------------")
+		for _, d := range data {
+			fmt.Printf("input %f | expected %f | predicted %f \n", d[0], d[1], d[0]*w)
+		}
+	} else {
+		eps := 1e-3
+		for i := 0; i < 100; i++ {
+			c := cost_twice_eps(data, w)
+			dw := (cost_twice_eps(data, w+eps) - c) / eps
 
-	for _, d := range data {
-		fmt.Printf("input %f | expected %f | predicted %f \n", d[0], d[1], d[0]*w)
+			w -= lr * dw
+			fmt.Printf("cost %f | weight %f\n", cost_twice_derivative(data, w), w)
+		}
+
+		fmt.Println("-----------FINAL RESULTS------------")
+		for _, d := range data {
+			fmt.Printf("input %f | expected %f | predicted %f \n", d[0], d[1], d[0]*w)
+		}
 	}
 }
 
-func cost_twice(data [][]float64, w float64) float64 {
+func cost_twice_derivative(data [][]float64, w float64) float64 {
 	result := 0.0
 	for _, d := range data {
 		x := d[0]
 		y := d[1]
 
 		dw := 2 * (x*w - y) * x
+		result += dw
+	}
+
+	result = result / float64(len(data))
+
+	return result
+}
+
+func cost_twice_eps(data [][]float64, w float64) float64 {
+	result := 0.0
+	for _, d := range data {
+		x := d[0]
+		y := x * w
+
+		dw := y * y
 		result += dw
 	}
 
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "gobraincells/gates"
 
 func main() {
-	gates.Twice()
+	gates.Twice(true)
 }
